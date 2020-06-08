6fecc8c075069eec5d77b6eccf9b5763b2469a27
Aleksandr <aleksandros-777@mail.ru>
2020-06-08 08:52:39 +0300

    changed django version to 2.2.13

--- users/urls.py
+++ users/urls.py
@@ -7,15 +7,13 @@ from . import views
 
 app_name = 'users'
 
-login = LoginView.as_view(template_name='users/login.html')
-
 urlpatterns = [
     # Страница входа
-    path('login/', login, name='login'),
-    
+    path('login/', LoginView.as_view(template_name='users/login.html'), name='login'),
+
     # Страница выхода
     path('logout/', views.logout_view, name='logout'),
-    
+
     # Страница регистрации
     path('register/', views.register, name='register'),
 ]
